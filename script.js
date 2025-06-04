17f6de885c37bd472ca17f787a95384beead06f0
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-06-04 07:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let fname=document.getElementbyId(fname).value;
+let lname=document.getElementbyId(lname).value;
+let phone=document.getElementbyId(phone).value;
+let email=document.getElementbyId(email).value;
+let btn=document.getElementbyId(btn);
+btn.addEventListener("submit",function (e){
+	e.preventDefault();
+	let mess=fname+" "+lname+" "+phone+" "+email;
+	alert(mess);
+});
